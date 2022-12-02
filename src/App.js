2ad313b35a16ce17b3823dc6ac2848a310ebd279
Ivan Kaminski <ivankaminskii323@gmail.com>
2022-12-02 15:26:31 +0100

    task1: new version

--- src/App.js
+++ src/App.js
@@ -1,30 +1,79 @@
-import logo from './404.png';
+import React from 'react';
+import logo404 from './404.png';
 import './App.css';
+
+const logo = <span className='logo'><span className='logo-bold'>netflix</span>roulette</span>
 function App() {
   return (
-    <body>
-
-      <header>
-        <div class="container">
-          <p class="text"><span class="test">netflix</span>roulette</p>
-        </div>
-      </header>
-      <div class="main">
-        <div class="container">
-          <h1 id="white" class="text">Page Not Found</h1>
-          <img className="displayed" alt="404 not found" src={logo} />
-          <button role="button">
-            <h3>Go Back To Home</h3>
-          </button>
-        </div>
-        <div class="container">
-
-        </div>
+    <React.Fragment>
+      <div className="header">
+        {logo}
       </div>
-      <div class="footer">
-        <h2 class="footertext">NetflixRoulette</h2>
+      <div className="main">
+        <h1 id="not-found" className="text">Page Not Found</h1>
+        <img alt="404 not found" src={logo404} />
+        <button role="button">
+          <span>Go Back To Home</span>
+        </button>
       </div>
-    </body>
+      <div className="footer">
+        {logo}
+      </div>
+    </React.Fragment>
   );
 }
+
+class App1 extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="header">
+          <p className="text"><span class="test">netflix</span>roulette</p>
+        </div>
+        <div class="main">
+          <div class="container">
+            <h1 id="white-text" class="text">Page Not Found</h1>
+            <img className="displayed" alt="404 not found" src={logo} />
+            <button role="button">
+              <h3>Go Back To Home</h3>
+            </button>
+          </div>
+          <div class="container">
+
+          </div>
+        </div>
+        <div class="footer">
+          <h2 class="footertext">NetflixRoulette</h2>
+        </div>
+      </div>
+    );
+  }
+}
+class App2 extends React.PureComponent {
+  render() {
+    return (
+      <div>
+        <div class="header">
+          <p class="text"><span class="test">netflix</span>roulette</p>
+        </div>
+        <div class="main">
+          <div class="container">
+            <h1 id="white-text" class="text">Page Not Found</h1>
+            <img className="displayed" alt="404 not found" src={logo} />
+            <button role="button">
+              <h3>Go Back To Home</h3>
+            </button>
+          </div>
+          <div class="container">
+          </div>
+        </div>
+        <div class="footer">
+          <h2 class="footertext">NetflixRoulette</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+
 export default App;
