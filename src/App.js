3725bfe42f7e6a843b82060dc57206f7771273e6
Ivan Kaminski <ivankaminskii323@gmail.com>
2022-12-02 16:34:35 +0100

    implementation React.createElement

--- src/App.js
+++ src/App.js
@@ -1,76 +1,83 @@
 import React from 'react';
 import logo404 from './404.png';
 import './App.css';
-
+const createLogoElement = React.createElement('span', { className: 'logo' }, React.createElement('span', { className: 'logo-bold' }, 'netflix'), 'roulette')
 const logo = <span className='logo'><span className='logo-bold'>netflix</span>roulette</span>
+
 function App() {
   return (
-    <React.Fragment>
-      <div className="header">
-        {logo}
-      </div>
-      <div className="main">
-        <h1 id="not-found" className="text">Page Not Found</h1>
-        <img alt="404 not found" src={logo404} />
-        <button role="button">
-          <span>Go Back To Home</span>
-        </button>
-      </div>
-      <div className="footer">
-        {logo}
-      </div>
-    </React.Fragment>
+    React.createElement(React.Fragment, null,
+      React.createElement('div', { className: 'header' },
+        createLogoElement),
+      React.createElement('div', { className: 'main' },
+        React.createElement('h1', { id: 'not-found', className: 'text' }, 'Page Not Found'
+        ),
+        React.createElement('img', { alt: '404 not found', src: logo404  }
+        ),
+        React.createElement('button', { role: 'button' }, 'Go Back To Home'
+        )),
+      React.createElement('div', {className:'footer'},
+       createLogoElement)
+    )
   );
+  // return (
+  //   <React.Fragment>
+  //     <div className="header">
+  //       {logo}
+  //     </div>
+  //     <div className="main">
+  //       <h1 id="not-found" className="text">Page Not Found</h1>
+  //       <img alt="404 not found" src={logo404} />
+  //       <button role="button">
+  //         Go Back To Home
+  //       </button>
+  //     </div>
+  //     <div className="footer">
+  //       {logo}
+  //     </div>
+  //   </React.Fragment>
+  // );
 }
 
 class App1 extends React.Component {
   render() {
     return (
-      <div>
+      <React.Fragment>
         <div className="header">
-          <p className="text"><span class="test">netflix</span>roulette</p>
+          {logo}
         </div>
-        <div class="main">
-          <div class="container">
-            <h1 id="white-text" class="text">Page Not Found</h1>
-            <img className="displayed" alt="404 not found" src={logo} />
-            <button role="button">
-              <h3>Go Back To Home</h3>
-            </button>
-          </div>
-          <div class="container">
-
-          </div>
+        <div className="main">
+          <h1 id="not-found" className="text">Page Not Found</h1>
+          <img alt="404 not found" src={logo404} />
+          <button role="button">
+            <span>Go Back To Home</span>
+          </button>
         </div>
-        <div class="footer">
-          <h2 class="footertext">NetflixRoulette</h2>
+        <div className="footer">
+          {logo}
         </div>
-      </div>
+      </React.Fragment>
     );
   }
 }
 class App2 extends React.PureComponent {
   render() {
     return (
-      <div>
-        <div class="header">
-          <p class="text"><span class="test">netflix</span>roulette</p>
+      <React.Fragment>
+        <div className="header">
+          {logo}
         </div>
-        <div class="main">
-          <div class="container">
-            <h1 id="white-text" class="text">Page Not Found</h1>
-            <img className="displayed" alt="404 not found" src={logo} />
-            <button role="button">
-              <h3>Go Back To Home</h3>
-            </button>
-          </div>
-          <div class="container">
-          </div>
+        <div className="main">
+          <h1 id="not-found" className="text">Page Not Found</h1>
+          <img alt="404 not found" src={logo404} />
+          <button role="button">
+            <span>Go Back To Home</span>
+          </button>
         </div>
-        <div class="footer">
-          <h2 class="footertext">NetflixRoulette</h2>
+        <div className="footer">
+          {logo}
         </div>
-      </div>
+      </React.Fragment>
     );
   }
 }
