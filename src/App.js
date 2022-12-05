8100be41039d361a0f0cc3a631a2bde65bd1c415
Ivan Kaminski <ivankaminskii323@gmail.com>
2022-12-05 09:49:44 +0100

    createComponent

--- src/App.js
+++ src/App.js
@@ -5,38 +5,38 @@ const createLogoElement = React.createElement('span', { className: 'logo' }, Rea
 const logo = <span className='logo'><span className='logo-bold'>netflix</span>roulette</span>
 
 function App() {
-  // return (
-  //   React.createElement(React.Fragment, null,
-  //     React.createElement('div', { className: 'header' },
-  //       createLogoElement),
-  //     React.createElement('div', { className: 'main' },
-  //       React.createElement('h1', { id: 'not-found', className: 'text' }, 'Page Not Found'
-  //       ),
-  //       React.createElement('img', { alt: '404 not found', src: logo404  }
-  //       ),
-  //       React.createElement('button', { role: 'button' }, 'Go Back To Home'
-  //       )),
-  //     React.createElement('div', {className:'footer'},
-  //      createLogoElement)
-  //   )
-  // );
   return (
-    <React.Fragment>
-      <div className="header">
-        {logo}
-      </div>
-      <div className="main">
-        <h1 id="not-found" className="text">Page Not Found</h1>
-        <img alt="404 not found" src={logo404} />
-        <button role="button">
-          Go Back To Home
-        </button>
-      </div>
-      <div className="footer">
-        {logo}
-      </div>
-    </React.Fragment>
+    React.createElement(React.Fragment, null,
+      React.createElement('div', { className: 'header' },
+        createLogoElement),
+      React.createElement('div', { className: 'main' },
+        React.createElement('h1', { id: 'not-found', className: 'text' }, 'Page Not Found'
+        ),
+        React.createElement('img', { alt: '404 not found', src: logo404 }
+        ),
+        React.createElement('button', { role: 'button' }, 'Go Back To Home'
+        )),
+      React.createElement('div', { className: 'footer' },
+        createLogoElement)
+    )
   );
+  // return (
+  //   <React.Fragment>
+  //     <div className="header">
+  //       {logo}
+  //     </div>
+  //     <div className="main">
+  //       <h1 id="not-found" className="text">Page Not Found</h1>
+  //       <img alt="404 not found" src={logo404} />
+  //       <button role="button">
+  //         Go Back To Home
+  //       </button>
+  //     </div>
+  //     <div className="footer">
+  //       {logo}
+  //     </div>
+  //   </React.Fragment>
+  // );
 }
 
 class App1 extends React.Component {
