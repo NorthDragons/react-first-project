ecbc61b3ffada55e04c6bb0c431883b9b259906a
Ivan Kaminski <ivankaminskii323@gmail.com>
2023-01-09 11:47:45 +0100

    remove func

--- src/components/tool-bar/filter/Filter.jsx
+++ src/components/tool-bar/filter/Filter.jsx
@@ -6,45 +6,6 @@ function Filter() {
     let [genres, setGenres] = useState({
         all: true,
     })
-
-    const switchActive = (e) => {
-        switch (e.target.value) {
-            case "all":
-                setGenres({
-                    all: true,
-                })
-                break;
-            case "documentary":
-                setGenres({
-                    documentary: true,
-                })
-                break;
-            case "comedy":
-                setGenres({
-                    comedy: true,
-                })
-                break;
-            case "horror":
-                setGenres({
-                    horror: true,
-                })
-                break;
-            case "crime":
-                setGenres({
-                    crime: true,
-                })
-                break;
-            default:
-                setGenres({
-                    all: true,
-                    documentary: true,
-                    comedy: true,
-                    horror: true,
-                    crime: true,
-                })
-        }
-    }
-
     return (
         <div className="filter">
             <ul className="filter__button-group">
